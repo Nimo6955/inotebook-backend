74e2282d9c90663c49c95a678608d613eda7b90b
Sukhpal <singhsukhpal20012001@gmail.com>
2024-01-18 16:36:33 +0530

    changed mongoURI

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = `mongodb://127.0.0.1:27017`
+const mongoURI = `mongodb+srv://Cluster0:8RS0oCQ6gl7E2W8U@cluster0.dfj0em4.mongodb.net/inotebookData`
 
 const connectToMongo = () => {
         mongoose.connect(mongoURI, () => {
